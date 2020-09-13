1184b23c2127fababa14ef8eb244efa248b8c982
ayorich <kayodele17@yahoo.com>
2020-09-13 23:34:17 +0100

    eslint & prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, { FC, ReactElement } from "react";
-import "./App.css";
+import React, { FC, ReactElement } from 'react';
+import './App.css';
 
 const App: FC = (): ReactElement => {
-  return (
-    <div className="app">
-      <h1>Hello, welcome to VidVote application</h1>
-      <button>click me</button>
-    </div>
-  );
+    console.log('error');
+    return (
+        <div className="app">
+            <h1>Hello, welcome to VidVote application</h1>
+            <button>click me</button>
+        </div>
+    );
 };
 
 export default App;
